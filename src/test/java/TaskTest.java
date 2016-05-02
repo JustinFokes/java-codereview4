bf1e3daa76157f74ab6841ac8bb65b4d364f619d
kendra <kendra.dunham@gmail.com>
2016-05-02 10:05:01 -0700

    initial commit

---
+++ src/test/java/TaskTest.java
@@ -0,0 +1,77 @@
+import java.time.LocalDateTime;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TaskTest {
+
+  @Before public void setUp(){
+    DB.sql2o = new Sql2o("jdb:postgresql://localhost:5432/to_do_test", null, null);
+  }
+
+  @After
+    public void tearDown() {
+      try(Connection con = DB.sql2o.open()){
+        String sql = "DELETE FROM tasks *;";
+        con.createQuery(sql).executeUpdate();
+      }
+    }
+
+  @Test
+  public void task_instantiatesCorrectly_true() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(true, myTask instanceof Task);
+  }
+
+  @Test
+  public void task_instantiatesWithDescription_String() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals("Mow the lawn", myTask.getDescription());
+  }
+
+  @Test
+  public void isCompleted_isFalseAfterInstantiation_false() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(false, myTask.isCompleted());
+  }
+
+  @Test
+  public void getCreatedAt_instantiatesWithCurrentTime_today() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfTask_true() {
+    Task firstTask = new Task("Mow the lawn");
+    Task secondTask = new Task("Buy groceries");
+    assertTrue(Task.all().contains(firstTask));
+    assertTrue(Task.all().contains(secondTask));
+  }
+
+  @Test
+  public void clear_emptiesAllTasksFromArrayList_0() {
+    Task myTask = new Task("Mow the lawn");
+    Task.clear();
+    assertEquals(Task.all().size(), 0);
+  }
+
+  @Test
+  public void getId_tasksInstantiateWithAnID_1() {
+    Task.clear();  // THIS TEST WILL FAIL WITHOUT THIS LINE!
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(1, myTask.getId());
+  }
+
+  @Test
+  public void find_returnsTaskWithSameId_secondTask() {
+    Task firstTask = new Task("Mow the lawn");
+    Task secondTask = new Task("Buy groceries");
+    assertEquals(Task.find(secondTask.getId()), secondTask);
+  }
+
+  @Test
+  public void find_returnsNullWhenNoTaskFound_null() {
+    assertTrue(Task.find(999) == null);
+  }
+
+}
