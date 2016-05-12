3c27dfea1cae7dcdceca47f989c682cc4d730b8a
justin fokes and sadio ali <j.k.fokes@gmail.com>
2016-05-12 10:40:18 -0700

    front end work

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,5 +13,43 @@ public class App {
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/users/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/sign_up.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/user/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String userName = request.queryParams("name");
+      User newUser = new User(userName);
+      newUser.save();
+      model.put("users", User.all());
+      response.redirect("/user/" + newUser.getId());
+      return null;
+      });
+
+    get("/user/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      User newUser = User.find(Integer.parseInt(request.params(":id")));
+      model.put("thisUser", newUser);
+      model.put("users", User.all());
+      model.put("template", "templates/user_home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/users", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("users", User.all());
+      model.put("template", "templates/users.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/recipes/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/recipe_form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
