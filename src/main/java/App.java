00e58c809a271eaf8b2ace1241dbc3821bb6b7af
Justin Fokes <j.k.fokes@gmail.com>
2016-05-12 23:26:23 -0700

    implementing basic front end: will test integration next

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,7 +10,70 @@ public class App {
 
     get("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("bands", Band.all());
       model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+     
+    get("/bands/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/band_form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/venues/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/venue_form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/band/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String bandName = request.queryParams("name");
+      Band newBand = new Band(bandName);
+      newBand.save();
+      model.put("band", newBand);
+      model.put("bands", Band.all());
+      response.redirect("/bands/" + newBand.getId());
+      return null;
+      });
+
+    post("/venue/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String venueName = request.queryParams("name");
+      Venue newVenue = new Venue(venueName);
+      newVenue.save();
+      model.put("venue", newVenue);
+      model.put("venues", Venue.all());
+      response.redirect("/venues/" + newVenue.getId());
+      return null;
+      });
+
+    get("/bands/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/bands.vtl");
+      model.put("bands", Band.all());
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/venues/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/venues.vtl");
+      model.put("venues", Venue.all());
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/bands", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/bands.vtl");
+      model.put("bands", Band.all());
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/venues", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/venues.vtl");
+      model.put("venues", Venue.all());
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
